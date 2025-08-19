642e7556b99cfca68ecf3c42597e21323f28b4f3
Karel Cancara <karel.cancara@gmail.com>
2025-08-19 23:20:14 +0200

    Stop redirecting

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://localhost:4321/", 
   i18n: {
     defaultLocale: "en",
     locales: ["en", "sv"],
